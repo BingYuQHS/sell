9200a679244ccdd0774818be1f280e1e0e78511b
秦华山 <1756217895@qq.com>
2018-09-03 22:01:49 +0800

    日志使用（@Slf4j注解方式）

--- src/test/java/com/flamingo/sell/LoggerTest.java
+++ src/test/java/com/flamingo/sell/LoggerTest.java
@@ -1,25 +1,26 @@
 package com.flamingo.sell;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.slf4j.event.Level; //日志级别查看
+
 /**
  * 日志测试
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class LoggerTest {
-
-    private final Logger log = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test1() {
+        String name = "qinhs";
+        int age = 21;
         log.debug("debug...");
         log.info("info...");
+        log.info("name: {}, age: {}",name,age);
         log.error("error...");
     }
 }
