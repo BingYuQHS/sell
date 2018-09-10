73099997971f041bd0ec29790c53d0b15326f995
秦华山 <1756217895@qq.com>
2018-09-10 22:18:29 +0800

    lombok依赖@Data注解省略get,set,toString方法

--- src/main/java/com/flamingo/sell/model/entity/ProductCategory.java
+++ src/main/java/com/flamingo/sell/model/entity/ProductCategory.java
@@ -1,5 +1,6 @@
 package com.flamingo.sell.model.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Proxy;
 
@@ -17,6 +18,7 @@ import java.util.Date;
 @JsonIgnoreProperties(value={"hibernateLazyInitializer","handler","fieldHandler"})
 @Entity
 @DynamicUpdate
+@Data //lombok可省略get,set,toString方法
 public class ProductCategory {
 
     /** 类目id. */
@@ -36,54 +38,20 @@ public class ProductCategory {
     /** 更新时间. */
     private Date updateTime;
 
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
+    /***
+     * 自定义构造方法
+     * @param categoryName
+     * @param categoryType
+     */
+    public ProductCategory(String categoryName, Integer categoryType) {
         this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
         this.categoryType = categoryType;
     }
 
-    public Date getCreateTime() {
-        return createTime;
-    }
+    /***
+     * 默认构造方法
+     */
+    public ProductCategory() {
 
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
     }
 }
