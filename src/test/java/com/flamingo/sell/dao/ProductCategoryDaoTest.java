bd6b1ebbf52e78fba50b24b0b308e317a9232745
秦华山 <1756217895@qq.com>
2018-09-10 22:19:28 +0800

    修改ProductCategoryDaoTest,使用@Transactional事务注解

--- src/test/java/com/flamingo/sell/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/flamingo/sell/dao/ProductCategoryDaoTest.java
@@ -1,11 +1,13 @@
 package com.flamingo.sell.dao;
 
 import com.flamingo.sell.model.entity.ProductCategory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,21 +23,19 @@ public class ProductCategoryDaoTest {
     }
 
     @Test
+    @Transactional
     public void saveTest() {
-        ProductCategory productCategory = new ProductCategory();
-        productCategory.setCategoryName("女生最爱榜");
-        productCategory.setCategoryType(3);
-        productCategoryDao.save(productCategory);
+        ProductCategory productCategory = new ProductCategory("女生最爱榜",4);
+        ProductCategory result = productCategoryDao.save(productCategory);
+        Assert.assertNotNull(result);
+        // Assert.assertNotEquals(null,result);
     }
 
-    /**
-     * 先查出记录，再更新记录，只更新set的数据，没有动态的更新时间
-     */
     @Test
+    @Transactional
     public void updateOne() {
         ProductCategory productCategory = productCategoryDao.findOne(2);
         productCategory.setCategoryName("男生最爱榜");
-        productCategory.setCategoryType(3);
         productCategoryDao.save(productCategory);
     }
 }
