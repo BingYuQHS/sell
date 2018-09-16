8b4e6cb23df53aabe927430c8b726b4c358facb4
秦华山 <1756217895@qq.com>
2018-09-16 15:19:28 +0800

    买家商品的Service层开发并测试

---
+++ src/main/java/com/flamingo/sell/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.flamingo.sell.service;
+
+import com.flamingo.sell.model.entity.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/***
+ * 商品service
+ */
+public interface ProductService {
+
+    ProductInfo findOne(String productId);
+
+    /***
+     * C端：查询所有在架商品列表
+     * @return
+     */
+    List<ProductInfo> findUpAll();
+
+    /***
+     * 管理端：分页查询
+     * @return
+     */
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    /***
+     * 更新/保存
+     * @param productInfo
+     * @return
+     */
+    ProductInfo save(ProductInfo productInfo);
+
+    /***
+     * TODO: 取消订单时减加库存
+     */
+
+
+    /***
+     * TODO：下订单减库存
+     */
+}
---
+++ src/main/java/com/flamingo/sell/service/impl/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.flamingo.sell.service.impl;
+
+import com.flamingo.sell.dao.ProductInfoDao;
+import com.flamingo.sell.enums.ProductStatusEnum;
+import com.flamingo.sell.model.entity.ProductInfo;
+import com.flamingo.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/***
+ * 商品
+ * service层实现
+ */
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductInfoDao productInfoDao;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return productInfoDao.findOne(productId);
+    }
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return productInfoDao.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return productInfoDao.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return productInfoDao.save(productInfo);
+    }
+}
